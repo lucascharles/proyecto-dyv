100bdbc80529b103a610c3053b6ad23834b7cc51
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-01-29 16:16:49 +0000

    modificaciones visuales en la presentacion de errores.

--- views/log_error_carga.php
+++ views/log_error_carga.php
@@ -11,7 +11,10 @@
 
     <script language="javascript">
 
-		
+		function imprimirLog()
+		{
+			window.print();
+		}
 		function salir()
 		{
 			window.parent.salir();
@@ -36,6 +39,9 @@
 <body id="datos">
 <form name="frmcargamasiva" action="" method="post" >
 <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" >	
+	<tr class="">
+      <td  align="right" colspan="3"><img src="images/print.gif" style="cursor:pointer;" onclick="imprimirLog()" title="Imprimir Log Errores" /></td>  
+    </tr>
 	<tr class="cabecera_listado">
       <th width="15" align="center"></th>
       <th align="left" colspan="2"><font class="titulolistado">Resultados</font></th>  
@@ -79,16 +85,68 @@
       <th width="15" align="center"></th>
       <th align="left" colspan="2"><font class="titulolistado">Detalles</font></th>  
     </tr>
+    <tr class="">
+      <td height="10" align="center" colspan="3"></td>
+      
+    </tr>
      <tr>
         <td align="left" colspan="3" height="15" class="etiqueta_form">
+        	<table border="0" cellpadding="2" cellspacing="2" width="100%" align="center" >
+    
+            	<tr bgcolor="#F3F3F3">
+                	<td width="10%" height="15" align="center">
+                    	FILA
+                    </td>
+                    <td width="60%" align="center">
+                    	TIPO ERROR
+                    </td>
+                    <td width="30%" align="center">
+                    	ARCHIVO
+                    </td>
+                </tr>
+                        <tr>
+                	<td height="5" colspan="3">
+                    </td>
+                </tr>
     <?
 		
 		for($j=0; $j<$logerror_det->get_count(); $j++) 
 		{
 			$datoTmp = &$logerror_det->items[$j];
-			echo("<br> Fila: ".$datoTmp->get_data("fila")." | Tipo error: ".$datoTmp->get_data("id_tipo_error")." | Archivo: ".$datoTmp->get_data("archivo"));
+			$descrip_error = "";
+			for($i=0; $i<$coltipoerror->get_count(); $i++) 
+			{	
+				$datoe = &$coltipoerror->items[$i];
+				if($datoTmp->get_data("id_tipo_error") == $datoe->get_data("id"))
+				{
+					$descrip_error = $datoe->get_data("error");
+				}
+			}
+			?>
+            	
+                <tr bgcolor="#F3F3F3">
+                    <td width="10%"  align="center" height="15">
+                    	<font color='#0000FF'><? echo($datoTmp->get_data("fila")); ?></font>
+                    </td>
+                    <td width="60%" align="center">
+                    	<font color='#0000FF'><? echo($descrip_error); ?></font>
+                    </td>
+
+                    <td width="30%"  align="center">
+                    	<font color='#0000FF'><? echo($datoTmp->get_data("archivo")); ?></font>
+                    </td>
+                </tr>
+                <tr bgcolor="">
+                	<td height="5" colspan="3">
+                    </td>
+                </tr>
+                
+            <?
+			//echo("<br> Fila: <font color='#0000FF'>".$datoTmp->get_data("fila")."</font> | Tipo error: <font color='#0000FF'>".$descrip_error."</font> | Archivo: <font color='#0000FF'>".$datoTmp->get_data("archivo")."</font>");
 		}
 	?>
+    	
+    		</table>
     	</td>
     </tr>
     <?
