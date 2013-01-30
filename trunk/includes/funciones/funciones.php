f224d16376afe60ce5faef0a1e7c8249fc571191
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-01-30 02:56:33 +0000

    se agrego la funcion formatoFecha()

--- trunk/includes/funciones/funciones.php
+++ trunk/includes/funciones/funciones.php
@@ -15,5 +15,74 @@
 		
 		return $id;
 	}
+	
+	function formatoFecha($fechavieja, $formatoOrigen, $formatoDestino)
+	{
+		$resp = "";
+		if(strlen($fechavieja) > 10)
+		{
+			$tiempo = substr($fechavieja, 10);
+		
+			$fechavieja = substr($fechavieja, 0, 10);
+		}
+		if(strpos($fechavieja,"-") <> false)
+		{
+			list($a,$b,$c) = explode("-", $fechavieja);
+		}
+		else
+		{
+			list($a,$b,$c) = explode("/", $fechavieja);
+		}
+		
+		if(strlen($a)<2)
+		{
+			$a = str_pad($a, 2, "0", STR_PAD_LEFT);
+		}
+		if(strlen($b)<2)
+		{
+			$b = str_pad($b, 2, "0", STR_PAD_LEFT);
+		}
+		if(strlen($c)<2)
+		{
+			$c = str_pad($c, 2, "0", STR_PAD_LEFT);
+		}
+		
+		if(strpos($fechavieja,"-") <> false)
+		{
+			$fechavieja = $a."-".$b."-".$c;
+		}
+		else
+		{
+			$fechavieja = $a."/".$b."/".$c;
+		}
+		
+		
+		if($formatoOrigen == "yyyy-mm-dd" && $formatoDestino == "dd/mm/yyyy")
+		{
+    		list($a,$m,$d) = explode("-", $fechavieja);
+			$resp = $d."/".$m."/".$a;
+		}
+		
+		if($formatoOrigen == "yyyy-mm-dd H:m:s" && $formatoDestino == "dd/mm/yyyy")
+		{
+		
+    		list($a,$m,$d) = explode("-", $fechavieja);
+			$resp = $d."/".$m."/".$a." ".$tiempo;
+		}
+		
+		if($formatoOrigen = "dd/mm/yyyy" && $formatoDestino == "yyyy-mm-dd")
+		{
+    		list($d,$m,$a) = explode("/", $fechavieja);
+			$resp = $a."-".$m."-".$d;
+		}
+		
+		
+		if($formatoOrigen == $formatoDestino)
+		{
+    		$resp = $fechavieja." ".$tiempo;
+		}
+				
+		return $resp;
+	}
 	  
 ?>
