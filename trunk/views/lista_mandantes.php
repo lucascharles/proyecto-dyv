9ecb040d6723464f96b0ccaa0e6d02c9595a7a6d
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-02-28 03:04:34 +0000

    modifiacion para limitar cantidad de datos de la grilla

--- trunk/views/lista_mandantes.php
+++ trunk/views/lista_mandantes.php
@@ -6,7 +6,34 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title></title>
      <link rel="stylesheet" href="css/general.css" type="text/css" />
+      <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
     <script language="javascript"> 
+		function verMasRegistros(id)
+		{
+			var datos = "controlador=Mandantes&accion=listar_mas_registros";
+			datos += "&des_int="+window.parent.document.getElementById("txtrut").value;
+			datos += "&desApel1="+window.parent.document.getElementById("txtPrimerApel").value;
+			datos += "&desNomb1="+window.parent.document.getElementById("txtPrimerNomb").value;
+			datos += "&id_partida="+id;
+			
+			$.ajax({
+					url: "index.php",
+					type: "GET",
+					data: datos,
+					cache: false,
+					success: function(res)
+					{
+						$("#btnvermas_"+id).hide("slow"); 
+						$("#masdatos_"+id).html(res); 
+						$("#masdatos_"+id).slideDown("slow"); 
+					},
+					error: function()
+					{
+						//alert("Ha ocurrido un error y no se ha podido agregar el registro.");
+					}
+				});
+		}
+		
 		function seleccionado(id)
 		{
 			window.parent.seleccionado(id);
@@ -26,14 +53,14 @@
 <body bgcolor="#FFFFFF">
 <table width="100%" cellpadding="2" cellspacing="2" align="center" border="0" bgcolor="#FFFFFF">
 	<tr class="cabecera_listado">
-    	<th width="15" align="center"></th>
-      <th align="center"><font class="titulolistado">Rut</font></th>
-      <th align="center"><font class="titulolistado">Apellido</font></th>
-      <th align="center"><font class="titulolistado">Nombre</font></th>
-      <th align="center"><font class="titulolistado">Banco 1</font></th>
-      <th align="center"><font class="titulolistado">Cta Cte1</font></th>
-      <th align="center"><font class="titulolistado">Banco2</font></th>
-      <th align="center"><font class="titulolistado">Cta Cte 2</font></th>   
+    	<th width="3%" align="center"></th>
+      <th align="center" width="17%"><font class="titulolistado">Rut</font></th>
+      <th align="center" width="15%"><font class="titulolistado">Apellido</font></th>
+      <th align="center" width="15%"><font class="titulolistado">Nombre</font></th>
+      <th align="center" width="15%"><font class="titulolistado">Banco 1</font></th>
+      <th align="center" width="10%"><font class="titulolistado">Cta Cte1</font></th>
+      <th align="center" width="15%"><font class="titulolistado">Banco2</font></th>
+      <th align="center" width="10%"><font class="titulolistado">Cta Cte 2</font></th>   
     </tr>
 	<?php
 
@@ -70,11 +97,11 @@
 	  ?>
       </td>
       
-      <td align="center"  class="dato_lista"><?php echo ($datoTmp->get_data("rut_mandante")."-".$datoTmp->get_data("dv_mandante"))?></td>      
-      <td align="center" class="dato_lista"><?php echo ($datoTmp->get_data("apellido")) ?></td>
-      <td align="center" class="dato_lista"><?php echo ($datoTmp->get_data("nombre")) ?></td>           
+      <td align="left"  class="dato_lista"><?php echo ($datoTmp->get_data("rut_mandante")."-".$datoTmp->get_data("dv_mandante"))?></td>      
+      <td align="left" class="dato_lista"><?php echo ($datoTmp->get_data("apellido")) ?></td>
+      <td align="left" class="dato_lista"><?php echo ($datoTmp->get_data("nombre")) ?></td>           
   
-      <td align="center" class="dato_lista">
+      <td align="left" class="dato_lista">
 	  <?php 
 	  for($i=0; $i<$colleccionBancos->get_count(); $i++) 
 	  {
@@ -87,8 +114,8 @@
 	  }
 	  ?>
       </td>
-      <td align="center" class="dato_lista"><?php echo ($datoTmp->get_data("cuenta_corriente1")) ?></td>      
-      <td align="center" class="dato_lista">
+      <td align="left" class="dato_lista"><?php echo ($datoTmp->get_data("cuenta_corriente1")) ?></td>      
+      <td align="left" class="dato_lista">
 	   <?php 
 	  for($i=0; $i<$colleccionBancos->get_count(); $i++) 
 	  {
@@ -101,14 +128,28 @@
 	  }
 	  ?>
       </td>
-      <td align="center" class="dato_lista"><?php echo ($datoTmp->get_data("cuenta_corriente2")) ?></td>
+      <td align="left" class="dato_lista"><?php echo ($datoTmp->get_data("cuenta_corriente2")) ?></td>
       <tr bgcolor="#FFFFFF" >
-    	<td colspan="19" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
+    	<td colspan="8" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
 	</tr>
 	<?php
 	}
+	$datoTmp = &$colleccionMandantes->items[($colleccionMandantes->get_count()-1)];
+	
+	if($cant_mas > 0)
+	{
+		
+	?>
+    <tr bgcolor="#FFFFFF">
+    	<td colspan="8" align="center">
+        <div id='btnvermas_<? echo($datoTmp->get_data("id_mandante")) ?>' onclick="verMasRegistros(<? echo($datoTmp->get_data("id_mandante")) ?>)" style="cursor:pointer;" >Ver mas </div></td>
+	</tr>
+    <?
+    }
 	?>
     
 </table>
+<div  mascom='masdatcom' id="masdatos_<? echo($datoTmp->get_data("id_mandante")) ?>" style="display:none;">
+    </div>
 </body>
 </html>
