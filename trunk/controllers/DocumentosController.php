ffd26bd6b801ad7b8017afa255b152e39f34daf8
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-02-28 03:27:14 +0000

    - modificacion de la funcion listar() para realizar la busqueda por filtros - se agrega metodo listar_mas_registros()

--- trunk/controllers/DocumentosController.php
+++ trunk/controllers/DocumentosController.php
@@ -95,15 +95,74 @@ class DocumentosController extends ControllerBase
 		$this->view->show("lista_documentos.php", $data);
 	}  
 	
+	public function listar_mas_registros($array)
+    {
+		require 'models/DocumentosModel.php';
+		$documentos = new DocumentosModel();
+		$dato = $documentos->getListaDocumentos("","",$array);
+
+		$datoTmp = &$dato->items[($dato->get_count()-1)];
+		$array["id_partida"] = $datoTmp->get_data("id_documento");
+		$datoAux = $documentos->getListaDocumentos("","",$array);
+		
+		$html = "<table width='100%' cellpadding='2' cellspacing='2' align='center' border='0' bgcolor='#FFFFFF'>";
+		
+		for($j=0; $j<$dato->get_count(); $j++) 
+		{
+			$datoTmp = &$dato->items[$j];
+			
+			$html .= "<tr bgcolor='#FFFFFF'>";
+    		$html .= "<td  width='3%'>";
+			$html .= "<input type='radio' id='".$datoTmp->get_data("id_documento")."' name='checktipdoc' value='' onclick='seleccionado(".$datoTmp->get_data("id_documento").")'>";
+			$html .= "</td>";
+			$html .= "<td align='left' width='12%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("id_documento")."</td>";
+			$html .= "<td align='left' width='10%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("ape1_deudor")." ".$datoTmp->get_data("ape2_deudor").$datoTmp->get_data("nom1_deudor")."</td>";
+			$html .= "<td align='left' width='10%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("nombre_mandante")."</td>";
+			$html .= "<td align='left' width='10%' class='dato_lista'>&nbsp;&nbsp;".formatoFecha($datoTmp->get_data("fecha_siniestro"),"dd-mm-yyyy","dd/mm/yyyy")."</td>";
+			$html .= "<td align='left' width='10%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("id_estado_doc")."</td>";
+			$html .= "<td align='left' width='10%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("numero_documento")."</td>";
+			$html .= "<td align='left' width='9%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("id_tipo_doc")."</td>";
+			$html .= "<td align='left' width='8%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("monto")."</td>";
+			$html .= "<td align='left' width='9%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("id_banco")."</td>";
+			$html .= "<td align='left' width='9%' class='dato_lista'>&nbsp;&nbsp;".$datoTmp->get_data("cta_cte")."</td>";
+			$html .= "</tr>";
+    		$html .= "<tr bgcolor='#FFFFFF'>";
+			$html .= "<td colspan='11' style='border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC;'></td>";
+			$html .= "</tr>";
+		
+		}
+		
+		$datoTmp = &$dato->items[($dato->get_count()-1)];
+		
+		if($datoAux->get_count() > 0)
+		{
+			$html .= "<tr bgcolor='#FFFFFF'>";
+    		$html .= "<td colspan='11' align='center'>";
+        	$html .= "<div id='btnvermas_".$datoTmp->get_data("id_documento")."' onclick='verMasRegistros(".$datoTmp->get_data("id_documento").")' style='cursor:pointer;'>Ver mas </div>";
+			$html .= "</td>";
+			$html .= "</tr>";
+	    }
+	    $html .= "</table>";
+		$html .= "<div  mascom='masdatcom' id='masdatos_".$datoTmp->get_data("id_documento")."' style='display:none;'>";
+	    $html .= "</div>";
+		
+		echo($html);
+	}
+	
 	public function listar($array)
     {
 		require 'models/DocumentosModel.php';
 		$documentos = new DocumentosModel();
 			
-		$dato = $documentos->getListaDocumentos("",$array["ident"]);
+		$dato = $documentos->getListaDocumentos("","",$array);
+		$datoTmp = &$dato->items[($dato->get_count()-1)];
 		
+		$array["id_partida"] = $datoTmp->get_data("id_documento");
+		$datoAux = $documentos->getListaDocumentos("","",$array);
+	
 		$data['nom_sistema'] = "SISTEMA DyV";
 		$data['colleccionDatosDocumentos'] = $dato;
+		$data['cant_mas'] = $datoAux->get_count();
 		
 		$this->view->show("lista_documentos.php", $data);
 	}    
