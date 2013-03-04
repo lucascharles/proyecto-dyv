733695652f58e009fe1196a2f4f8b7964be7e2fe
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-03-04 15:06:54 +0000

    ajustes para no pintar div para mas datos cuando no hay mas registros por paginar

--- views/lista_deudores.php
+++ views/lista_deudores.php
@@ -88,7 +88,14 @@
     }
 	?>
     </table>
+	  <?
+    if($cant_mas > 0)
+	{
+	?>
 	<div  mascom='masdatcom' id="masdatos_<? echo($datoTmp->get_data("id_deudor")) ?>" style="display:none;">
     </div>
+    <?
+    }
+	?>
 </body>
 </html>
