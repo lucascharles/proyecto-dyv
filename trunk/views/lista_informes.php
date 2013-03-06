fe0743a0c68fe39c7b0d26e6cf61318ccf8145c0
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-03-06 12:11:58 +0000

    - correccion en visualizacion de rut mandante - ajustes para seleccion de documentos y generacion de archivo excel

--- trunk/views/lista_informes.php
+++ trunk/views/lista_informes.php
@@ -13,14 +13,102 @@
 			window.parent.seleccionado(id);
 		}
 		
+		function seleccionar()
+		{
+			if(document.getElementById("chkTodos").checked == true)
+			{
+				var url = "index.php?controlador=Informes&accion=marcar&tipoInforme=";
+				url += window.parent.document.getElementById("selTipoInforme").value
+				url += "&idmandante="+window.parent.document.getElementById("id_mandante").value;
+				url += "&tipoDoc="+window.parent.document.getElementById("selTipoDoc").value
+			}
+			else
+			{
+				var url = "index.php?controlador=Informes&accion=listar&tipoInforme=";
+				url += window.parent.document.getElementById("selTipoInforme").value
+				url += "&idmandante="+window.parent.document.getElementById("id_mandante").value;
+				url += "&tipoDoc="+window.parent.document.getElementById("selTipoDoc").value
+			}
+			window.location = url;
+			
+		}
+		
+		function generarExcel()
+		{
+			var arrayInput = document.getElementsByTagName('input');
+	        var arrayID = new Array();
+    	    var j = 0;
+		
+	 		for(var i=0; i<arrayInput.length; i++)
+	 		{
+  				if(arrayInput[i].type == "checkbox")
+   				{
+                	if((arrayInput[i].name == "chkMarcar")&&(arrayInput[i].checked == true))
+            	 	{
+                		arrayID[j] = arrayInput[i].value ;
+                    	j = j+1;
+                 	}
+    			}	
+   			}        
+
+			if(j == 0)
+			{
+				return false;
+			}
+			var array_str = arrayID.toString();
+		
+			var url = "index.php?controlador=Informes&accion=generarExcel&tipoInforme=";
+				url += window.parent.document.getElementById("selTipoInforme").value
+				url += "&idmandante="+window.parent.document.getElementById("id_mandante").value;
+				url += "&tipoDoc="+window.parent.document.getElementById("selTipoDoc").value
+				url += "&iddocs="+array_str;
+				
+				window.location = url;
+		}
+		
 	</script>
 </head>
 <body bgcolor="#FFFFFF">
+<form name="frmlistadoinforme">
+<?
+$marcar = &$marcartodo;
+$title = "Marcar Todos";
+$checked = "";
+if($marcar == "S")
+{
+	$title = "Desmarcar Todos";
+	$checked = "checked";
+}
+?>
 <div id="datos" style="">
 
 <table width="100%" cellpadding="2" cellspacing="2" align="center" border="0" bgcolor="#FFFFFF">
+<tr class="">
+    	<th align="right" colspan="19" height="5" class="cabecera_listado">
+    
+        </th>
+    </tr>
+	<tr class="">
+    	<th align="left" colspan="19"  valign="middle">
+        	<table width="150" align="left" border="0" cellpadding="0"  cellspacing="0" onclick="generarExcel()" style="cursor:pointer; " title="Generar Excel">
+            	<tr>
+                	<td width="30">
+        <img src="images/excel.gif" />
+        			</td>
+                    <td align="left" class="etiqueta_form">
+        		&nbsp;Generar Excel
+                	</td>
+        		</tr>
+            </table>
+        </th>
+    </tr>
+    <tr class="">
+    	<th align="right" colspan="19" height="5" class="cabecera_listado">
+    
+        </th>
+    </tr>
 	<tr class="cabecera_listado">
-    	<th align="center"></th>
+    	<th align="center"><input type="checkbox" <? echo($checked) ?> name="chkTodos" id="chkTodos" title="<? echo($title) ?>" onclick="seleccionar()" /></th>
 		<th align="center"><font class="titulolistado">Mandatario</font></th>
         <th align="center"><font class="titulolistado">Deudor</font></th>
         <th align="center"><font class="titulolistado">Siniestro</font></th>
@@ -48,13 +136,13 @@
 			
 	?>
 	<tr bgcolor="#FFFFFF" >
-	<?php $marcar = &$marcartodo;
+	<?php 
 		if($marcar == "S"){ ?>
-    	<td align="left" class="dato_lista">&nbsp;&nbsp;<input type="checkbox" checked="checked" id="<? echo($datoTmp->get_data("id_documento")) ?>" name="checkinforme" value="" onclick=""></td>
+    	<td align="left" class="dato_lista">&nbsp;&nbsp;<input type="checkbox"  name="chkMarcar" checked="checked" id="<? echo($datoTmp->get_data("id_documento")) ?>" name="checkinforme" value="<? echo($datoTmp->get_data("id_documento")) ?>" onclick=""></td>
    	<?php }else{ ?>
-   		<td align="left" class="dato_lista">&nbsp;&nbsp;<input type="checkbox" id="<? echo($datoTmp->get_data("id_documento")) ?>" name="checkinforme" value="" onclick=""></td>
+   		<td align="left" class="dato_lista">&nbsp;&nbsp;<input type="checkbox" name="chkMarcar" id="<? echo($datoTmp->get_data("id_documento")) ?>" name="checkinforme" value="" onclick=""></td>
    	<?php } ?>
-		<td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("rut_mandatario")."-".$datoTmp->get_data("dv_mandatario")) ?></td>
+		<td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("rut_mandante")."-".$datoTmp->get_data("dv_mandante")) ?></td>
 		<td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("rut_deudor")."-".$datoTmp->get_data("dv_deudor")) ?></td>
         <td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("numero_siniestro")) ?></td>
         <td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("fecha_siniestro")) ?></td>
@@ -79,5 +167,6 @@
 
 </table>
 </div>
+</form>
 </body>
 </html>
