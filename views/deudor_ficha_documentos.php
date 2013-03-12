5929fe0d35400267b2d4b2e27258fc336eb537a1
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-03-12 01:49:15 +0000

    modificaciones para limitar la cantidad de registros a mostrar

--- views/deudor_ficha_documentos.php
+++ views/deudor_ficha_documentos.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>MVC - Modelo, Vista, Controlador - Jourmoly</title>
      <link rel="stylesheet" href="css/general.css" type="text/css" />
+      <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
      
     <script language="javascript"> 
 		function seleccionado(id)
@@ -14,7 +15,49 @@
 		}
 		function mostrarTotal(total)
 		{
-			document.getElementById("lblTotal").innerHTML = "Total: "+total;
+			
+			var datos = "controlador=Deudores&accion=buscar_total";
+			datos += "&ident="+window.parent.document.getElementById("ident").value;
+			datos += "&tipoperacion="+window.parent.document.getElementById("tipoperacion").value; 
+			
+			$.ajax({
+					url: "index.php",
+					type: "GET",
+					data: datos,
+					cache: false,
+					success: function(res)
+					{
+						document.getElementById("lblTotal").innerHTML = "Total: "+res;
+					},
+					error: function()
+					{
+						//alert("Ha ocurrido un error y no se ha podido agregar el registro.");
+					}
+				});
+		}
+		function verMasRegistros(id)
+		{
+			var datos = "controlador=Deudores&accion=listar_mas_fichadoc";
+			datos += "&id_partida="+id;
+			datos += "&ident="+window.parent.document.getElementById("ident").value;
+			datos += "&tipoperacion="+window.parent.document.getElementById("tipoperacion").value; 
+			
+			$.ajax({
+					url: "index.php",
+					type: "GET",
+					data: datos,
+					cache: false,
+					success: function(res)
+					{
+						$("#btnvermas_"+id).hide("slow"); 
+						$("#masdatos_"+id).html(res); 
+						$("#masdatos_"+id).slideDown("slow"); 
+					},
+					error: function()
+					{
+						//alert("Ha ocurrido un error y no se ha podido agregar el registro.");
+					}
+				});
 		}
 	</script>
 </head>
@@ -35,17 +78,17 @@
         </td>
     </tr>
 	<tr class="cabecera_listado" >
-		<th align="center"></th>
-		<th align="center"><font class="titulolistado">CORRELATIVO</font></th>
-        <th align="center"><font class="titulolistado">DEUDOR</font></th>
-		<th align="center"><font class="titulolistado">MANDANTE</font></th>
-        <th align="center"><font class="titulolistado">RECIBIDO</font></th>
-		<th align="center"><font class="titulolistado">ESTADO</font></th>
-        <th align="center"><font class="titulolistado">NRO. DOC.</font></th>
-        <th align="center"><font class="titulolistado">TIPO DOC.</font></th>
-		<th align="center"><font class="titulolistado">MONTO</font></th>
-        <th align="center"><font class="titulolistado">BANCO</font></th>
-        <th align="center"><font class="titulolistado">CTA. CTE.</font></th>
+		<th align="center" width='2%'></th>
+		<th align="center" width='17%'><font class="titulolistado">CORRELATIVO</font></th>
+        <th align="center" width='10%'><font class="titulolistado">DEUDOR</font></th>
+		<th align="center" width='10%'><font class="titulolistado">MANDANTE</font></th>
+        <th align="center" width='10%'><font class="titulolistado">RECIBIDO</font></th>
+		<th align="center" width='10%'><font class="titulolistado">ESTADO</font></th>
+        <th align="center" width='10%'><font class="titulolistado">NRO. DOC.</font></th>
+        <th align="center" width='8%'><font class="titulolistado">TIPO DOC.</font></th>
+		<th align="center" width='5%'><font class="titulolistado">MONTO</font></th>
+        <th align="center" width='10%'><font class="titulolistado">BANCO</font></th>
+        <th align="center" width='8%'><font class="titulolistado">CTA. CTE.</font></th>
     </tr>
 	<?php
 	$total = 0;
@@ -55,7 +98,7 @@
 		$total = $total + $datoTmp->get_data("monto");
 	?>
 	<tr bgcolor="#FFFFFF">
-    	<td><!--<input type="radio" id="<? //echo($datoTmp->get_data("id_documento")) ?>" name="checktipdoc" value="" onclick="seleccionado(<? //echo($datoTmp->get_data("id_documento")) ?>)">--></td>		
+    	<td></td>		
 		
 		<td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("id_documento")) ?></td>
 		<td align="left" class="dato_lista">&nbsp;&nbsp;<?php echo ($datoTmp->get_data("ape1_deudor")." ".$datoTmp->get_data("ape2_deudor").$datoTmp->get_data("nom1_deudor")) ?></td>
@@ -75,11 +118,34 @@
 	<?php
 	}
 	?>
-</table>
+
 <?
 	echo("<script language='javascript'>");
 	echo("mostrarTotal(".$total.");");
 	echo("</script>");
-?>
+
+	$datoTmp = &$colleccionDoc->items[($colleccionDoc->get_count()-1)];
+	if($cant_mas > 0)
+	{
+		
+	?>
+    <tr bgcolor="#FFFFFF">
+    	<td colspan="11" align="center">
+        <div id='btnvermas_<? echo($datoTmp->get_data("id_documento")) ?>' onclick="verMasRegistros(<? echo($datoTmp->get_data("id_documento")) ?>)" style="cursor:pointer;" >Ver mas </div></td>
+	</tr>
+    <?
+    }
+	?>
+    </table>
+    <?
+    if($cant_mas > 0)
+	{
+	?>
+	<div  mascom='masdatcom' id="masdatos_<? echo($datoTmp->get_data("id_documento")) ?>" style="display:none;">
+    </div>
+    <?
+    }
+	?>
+    
 </body>
 </html>
