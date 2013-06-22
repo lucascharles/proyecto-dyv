329353fc05bc3ef5c30fa9a55b6da887d78a33a2
seba.charles@gmail.com <seba.charles@gmail.com@0ae56715-e7fb-b30e-2ead-b92c8e2d47e2>
2013-06-22 00:29:32 +0000

    el tag meta con charset=utf-8"

--- views/log_error_carga.php
+++ views/log_error_carga.php
@@ -3,7 +3,7 @@
  
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
 	<title></title>
     <link id="theme" rel="stylesheet" type="text/css" href="css/general.css" title="theme" />
     <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
